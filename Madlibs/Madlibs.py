2e8e7ee2c07332595238a29600567d5095b704da
rodkevproxy <kevinrodasuk@gmail.com>
2026-07-17 14:19:39 +0100

    feat: (Madlid) Added the new folder and files terminating this small game based on input

---
+++ Madlibs/Madlibs.py
@@ -0,0 +1,17 @@
+##Maslibs game
+#So here we just have o construct something based on inputs given by the user 
+
+#I will use 5 inputs this time 
+
+input1 = input("Tell us the laptop you used on your first day of uni ")
+input2 = input("Tell us why that laptop was not the best ")
+input3 = input("Which laptop would iyou get instead of that one ")
+input4 = input("What is stopping you from getting it")
+input5 = input("How would you continue wheter the laptop is new or not")
+
+print (f"Today i was using my {input1} on my first day of uni ")
+print(f"Everything was good and normal until i found out that {input2}")
+print(f"Seeing this situation i started a plan and came out that the new best option for me would be a {input3}")
+print(f"While my current laptop works i do have to think about {input4}")
+print(f"So basically {input5}")
+
