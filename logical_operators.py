2b8d4dee5ce06f81530258d6b8c3f98da5d6a6bc
rodkevproxy <kevinrodasuk@gmail.com>
2026-07-19 23:13:46 +0100

    feat: logical operations basics

---
+++ logical_operators.py
@@ -0,0 +1,11 @@
+## This puts logical operator into practice 
+
+temp = 20 
+is_raining = True 
+
+if temp > 35 or temp < 0 or is_rainingL:
+    print("The outdoor event is cancelled")
+else: 
+    print("The outdoor event is still happening")
+
+
