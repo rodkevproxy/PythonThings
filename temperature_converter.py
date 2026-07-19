97c5c9126878268bd6878a48a871dce88241f200
rodkevproxy <kevinrodasuk@gmail.com>
2026-07-19 22:59:10 +0100

    feat: new temperature program added

--- temperature_converter.py
+++ temperature_converter.py
@@ -1,1 +1,15 @@
-##
+
+
+unit = input("Is the unit in Celsius or Fahrenheit (C/F)")
+
+temp = float(input("Enter the temperature: "))
+
+if unit == "C":
+    temp = round((9 * temp) / 32, 1)
+    print (f"The temperature in fahrenheit is {temp}")
+elif unit == "F": 
+    temp = round((temp - 32) * 5 / 9, 1)
+    print(f"The temperature in Celsious is {temp}")
+else: 
+    print(f"Unit {unit} is invalid")
+
