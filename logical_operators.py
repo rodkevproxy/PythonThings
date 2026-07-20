d7819f89d7678c6155512714ad84b4f738deefa4
rodkevproxy <kevinrodasuk@gmail.com>
2026-07-20 10:37:56 +0100

    feat: logical operations with temperature app done

--- logical_operators.py
+++ logical_operators.py
@@ -1,11 +1,24 @@
 ## This puts logical operator into practice 
 
 temp = 20 
-is_raining = True 
+is_sunny = True 
 
-if temp > 35 or temp < 0 or is_rainingL:
-    print("The outdoor event is cancelled")
-else: 
-    print("The outdoor event is still happening")
+if temp >= 28 and is_sunny:
+    print("It is hot outside")
+    print("It is sunny")
+elif temp <= 0 and is_sunny:
+    print("It is COLD outside")
+    print("It is SUNNY")
+elif 28 > temp > 0 and is_sunny:
+    print("Is WARM outside")
+    print("It is SUNNY")
+elif temp <= 0 and not is_sunny:
+    print("It is COLD outside")
+    print("It is CLOUDY")
+elif 28 > temp > 0 and not is_sunny:
+    print("It is WARM outside")
+    print("It is CLOUDY")
+
+
 
 
