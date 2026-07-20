e291dfb438d6ea6fc52148f66522bd90244bff98
rodkevproxy <kevinrodasuk@gmail.com>
2026-07-20 13:41:00 +0100

    feat: logical operations update'

--- logical_operators.py
+++ logical_operators.py
@@ -6,15 +6,19 @@ is_sunny = True
 if temp >= 28 and is_sunny:
     print("It is hot outside")
     print("It is sunny")
+    
 elif temp <= 0 and is_sunny:
     print("It is COLD outside")
     print("It is SUNNY")
+
 elif 28 > temp > 0 and is_sunny:
     print("Is WARM outside")
     print("It is SUNNY")
+
 elif temp <= 0 and not is_sunny:
     print("It is COLD outside")
     print("It is CLOUDY")
+
 elif 28 > temp > 0 and not is_sunny:
     print("It is WARM outside")
     print("It is CLOUDY")
