a65ab4185ca5677b9915608452c1017c94b36728
rodkevproxy <kevinrodasuk@gmail.com>
2026-07-21 22:00:36 +0100

    feat: New file for Format Specifiers

--- String_indexing.py
+++ String_indexing.py
@@ -1,3 +1,14 @@
 # indexing = accessing elements of a sequence using [] (indexing operator )
 #                        [start : end : step]
 
+# [::3] this will print every third character  
+# [:: -1] this would reverse a string 
+
+# reverse this credit card number 
+
+credit_number = "1234-5678-9098-7654"
+
+credit_number = credit_number[::-1] #This will reverse the entire string 
+
+last_digits = credit_number[-4:] #This would print the last four digits using negative indexing
+
