840184d39c3223e7d5850b8247a9acb32a2a2e1b
rodkevproxy <kevinrodasuk@gmail.com>
2026-07-21 22:21:17 +0100

    feat: Format Specifiers has been updated with some practical examples

---
+++ Format_specifiers.py
@@ -0,0 +1,15 @@
+# Format specifiers = {value:flags} format a value based on what flags are inserted 
+
+# .(number)f = round to that many decimal places (fixed point)
+# :(number) allocate that many spaces 
+# :03 allocate and zerp pad that many spaces 
+# :< left justify
+# :> rigth justify 
+# :^ center align 
+# :+ use a plus sign to indicate positive value
+# := place sign to the left most position 
+# :  inser a space before positive numbers 
+# :, comma separator 
+
+
+ 
