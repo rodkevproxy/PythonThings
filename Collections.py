11e296a07973e418dc71a062408e8230d745b627
rodkevproxy <kevinrodasuk@gmail.com>
2026-07-24 22:59:17 +0100

    Feat: rules and examples of sets, tuples and lists

--- Collections.py
+++ Collections.py
@@ -3,7 +3,32 @@
 # List [], Ordered and changeable. Duplicates ok
 # Ser {}, Unordered and immutable, but Add/Remove OK. NO duplicates 
 # Tuple (), Ordered and unchangeable. Duplicates OK. FASTER 
+
 # To list all the methods we can use the dir function, as well as the help function for something more descriptive 
+# We can append, which is adding an element to the end of a list
+# We can also use the method insert followed by the position we want to place the item at
+
+#List
+
+list = ["This is", "an", "Example of", "a", "Set"]
+
+# fruit[0] = pineapple 
+
+
+
+
+# Sets
+set = {"This is", "an", "Example of", "a", "Set"}
+
+# Tuple 
+
+tuple = ("This is", "an", "Example of", "a", "Set")
+
+
+
+
+# Pop method will remove what ever element is first but it will be random 
+
 
 
 
