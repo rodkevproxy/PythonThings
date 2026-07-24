47f48d6e13d7ce8ca34653093fb4b0bb21909d05
rodkevproxy <kevinrodasuk@gmail.com>
2026-07-24 12:00:29 +0100

    feat: Started with the collections types

---
+++ Collections.py
@@ -0,0 +1,9 @@
+# Is a variable used to store multiple values 
+
+# List [], Ordered and changeable. Duplicates ok
+# Ser {}, Unordered and immutable, but Add/Remove OK. NO duplicates 
+# Tuple (), Ordered and unchangeable. Duplicates OK. FASTER 
+# To list all the methods we can use the dir function, as well as the help function for something more descriptive 
+
+
+
