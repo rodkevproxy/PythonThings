7042c63e0c32449e5ca5cb490b535def79c89f1f
rodkevproxy <kevinrodasuk@gmail.com>
2026-07-26 22:22:45 +0100

    feat: New for loop and explanation

--- 2D_Lists.py
+++ 2D_Lists.py
@@ -2,4 +2,18 @@
 # 2D List is a list made out of lists
 
 
+#This 2D collection is a Lists
+
+groceries = [["Apple", "orange", "banana", "coconut"],
+             ["celery", "carrots", "potatoes"],
+             ["chicken", "fish", "turkey"]]
+
+# This can be also tuples made os sets, or use different ones depending on what is bet for the program
+
+
+for collection in groceries:
+    for food in collection:
+        print(food, end = " ")
+
+
 
