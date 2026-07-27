a77544a76972016536635ffc700ff2223e63a010
rodkevproxy <kevinrodasuk@gmail.com>
2026-07-27 23:33:28 +0100

    feat: Base variables of the quiz structure have been added

---
+++ Quiz Game/Quiz.py
@@ -0,0 +1,15 @@
+#Python quiz game
+
+questions = ("Best module on this year of uni",
+             "Worst module on this year of uni",
+             "Module that surprised you the most", 
+             "Module that did not have any impact",
+             "Module that you will take again" )
+
+options = ((), (), (), (), ())
+
+guesses = [] #This is a list because we need to append the names 
+score = 0 
+question_num = 0 
+
+
