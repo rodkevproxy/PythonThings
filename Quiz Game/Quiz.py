262923a9f8c962a3d10fc30d3edeaed5fe130b4f
rodkevproxy <kevinrodasuk@gmail.com>
2026-07-28 22:17:08 +0100

    feat: Added the options for the answers and decided to give the quiz a different approach

--- Quiz Game/Quiz.py
+++ Quiz Game/Quiz.py
@@ -6,10 +6,16 @@ questions = ("Best module on this year of uni",
              "Module that did not have any impact",
              "Module that you will take again" )
 
-options = ((), (), (), (), ())
+options = (("A. First Module", "B. Second Module", "C. Third Module", "D. Fourth Module "), 
+           ("A. First Module", "B. Second Module", "C. Third Module", "D. Fourth Module "),
+           ("A. First Module", "B. Second Module", "C. Third Module", "D. Fourth Module "),
+           ("A. First Module", "B. Second Module", "C. Third Module", "D. Fourth Module "), 
+           ("A. First Module", "B. Second Module", "C. Third Module", "D. Fourth Module "))
+
+#Because i made a mistake while planning i decided to turn the quiz into a game about how much does my uni friends know about me
+
 
 guesses = [] #This is a list because we need to append the names 
 score = 0 
 question_num = 0 
-
 
