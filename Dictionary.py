e2c36e2c8433bca4de4f34970a7b247c37646025
rodkevproxy <kevinrodasuk@gmail.com>
2026-07-30 23:10:49 +0100

    Feat: Added the basic form of a dictionary

--- Dictionary.py
+++ Dictionary.py
@@ -1,3 +1,6 @@
 # A dictionary is a collection of {key: value} pairs 
 # They are ordered and changeable, but do not allow duplicates
 
+capitals = {"UK": "London",
+            "USA": "Washington D.C.,"
+            "India": "New Delhi"}
