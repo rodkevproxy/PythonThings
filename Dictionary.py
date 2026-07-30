11432e9440519b4048f76cb44531544498afe29b
rodkevproxy <kevinrodasuk@gmail.com>
2026-07-30 23:17:03 +0100

    feat: Added the code line that will display a value from the dictionary

--- Dictionary.py
+++ Dictionary.py
@@ -4,3 +4,11 @@
 capitals = {"UK": "London",
             "USA": "Washington D.C.,"
             "India": "New Delhi"}
+
+# To see all the atributes and methos of the dictionary we can write the following lines print (dir(capitals)
+# This one shows helpfull commands too print (help(capitals))
+
+#To get one of the values from the dictionary: 
+print(capitals.get("USA"))
+
+
