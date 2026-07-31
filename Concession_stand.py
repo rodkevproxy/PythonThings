0ef904ba0a42e3a9e3c2ff0e16be825895d6f6c4
rodkevproxy <kevinrodasuk@gmail.com>
2026-07-31 21:09:06 +0100

    feat: Menu created using a dictionary

--- Concession_stand.py
+++ Concession_stand.py
@@ -1,1 +1,10 @@
 # Creating a concession stand using dictionaries 
+
+# First we create a menu using a dictionary
+
+menu = {"Pizza": 6.00,
+        "Burger": 10.0,
+        "Hot-Dog": 4.99,
+        "Ice cream": 4.99,
+        "Salad": 6.99}
+
