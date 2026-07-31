da73e8f407a42ed813d269ad8aacdd281b1c37d7
rodkevproxy <kevinrodasuk@gmail.com>
2026-07-31 23:25:37 +0100

    feat: New uses and methods that can be used with the random module

--- random_numbers.py
+++ random_numbers.py
@@ -6,3 +6,14 @@ import random
 
 number = random.randint(1, 6)
 print (number)
+
+low = 6 
+high = 60 
+options = ("rock", "paper", "scissors")
+
+number = random.randint(low, high)
+
+number = random.random() #This will return a point decimal number 
+
+option = random.choice(options)
+
