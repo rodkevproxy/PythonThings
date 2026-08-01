05b170fcdf338af293e508eac56e7386b512415a
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-01 23:10:16 +0100

    feat: Basic structure for the guessing game

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,2 +1,10 @@
 # Number guessing game to practice with the random module 
 
+import random 
+
+lowest_value = 1 
+highest_value = 100 
+answer = random.randint(lowest_value, highest_value)
+guesses = 0 
+is_running = True 
+
