b30e77b59272b541d09f08e146b4630bd7285273
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-02 21:09:04 +0100

    feat: While structure for input validation

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -13,3 +13,20 @@ print("Python Number Guessing Game")
 print(f"Select a number betweer {lowest_value} and {highest_value}")
 
 
+while is_running: 
+
+    guess = input("Enter your guess: ")
+
+    if guess.isdigit():
+        guess = int(guess)
+        guesses += 1 
+
+        if guess < lowest_value or guess > highest_value:
+             print("That guess is out of range")
+             print(f"Select a number betweer {lowest_value} and {highest_value}")
+       
+    else: 
+        print("invalid digit")
+        print(f"Select a number betweer {lowest_value} and {highest_value}")
+
+
