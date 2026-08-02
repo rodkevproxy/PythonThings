5a9e77c0cfbeee0751324b71eca0da8e0885c6de
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-02 22:17:10 +0100

    feat: If statements for the win conditions

--- rps_game.py
+++ rps_game.py
@@ -2,13 +2,26 @@
 # i will try my self first taking in consideration the things learned before 
 import random
 
-options = ["Rock",
+options = ("Rock",
            "Paper", 
-           "Scissors"]
+           "Scissors")
 
-option = random.choice(options)
-is_running = True 
+player = None
+answer = random.choice(options)
+ 
 
+while player not in options:
+    player = input("Enter a choice (Rock, Paper and Scissors)")
 
+print(f"Player choice was: {player}")
+print(f"Computer choice was: {answer}")
 
-
+# Win conditions 
+if player == answer: 
+    print("It is a tie")
+if player == "Rock" and answer == "Scissors":
+    print("You win")
+if player == "Paper" and answer == "Rock": 
+    print("You win")
+if player == "Scissors" and answer == "Paper":
+    print ("You win")
