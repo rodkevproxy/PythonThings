6383c9d336bda67505e4322e82059a09727a000e
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-02 21:47:32 +0100

    feat: New file for Rock, Paper and Scissors game

---
+++ rps_game.py
@@ -0,0 +1,14 @@
+# Rock Paper Scissors game 
+# i will try my self first taking in consideration the things learned before 
+import random
+
+options = ["Rock",
+           "Paper", 
+           "Scissors"]
+
+option = random.choice(options)
+is_running = True 
+
+
+
+
