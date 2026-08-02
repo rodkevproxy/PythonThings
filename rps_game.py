d2e17f18df9a9927d15438f608ff93a1000bf004
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-02 23:13:00 +0100

    feat: New condition that asks the user to continue playing or not

--- rps_game.py
+++ rps_game.py
@@ -8,25 +8,47 @@ options = ("Rock",
 
 player = None
 answer = random.choice(options)
- 
 
-while player not in options:
+game_running = True 
+
+while game_running:
+    player = None
+    answer = random.choice(options)
+
+    while player not in options:
     player = input("Enter a choice (Rock, Paper and Scissors)")
 
-print(f"Player choice was: {player}")
-print(f"Computer choice was: {answer}")
+    print(f"Player choice was: {player}")
+    print(f"Computer choice was: {answer}")
 
-# Win conditions 
-if player == answer: 
-    print("It is a tie")
-elif player == "Rock" and answer == "Scissors":
-    print("You win")
-elif player == "Paper" and answer == "Rock": 
-    print("You win")
-elif player == "Scissors" and answer == "Paper":
-    print ("You win")
+    # Win conditions 
 
-# Loose conditions 
+    if player == answer: 
+        print("It is a tie")
+    elif player == "Rock" and answer == "Scissors":
+        print("You win")
+    elif player == "Paper" and answer == "Rock": 
+        print("You win")
+    elif player == "Scissors" and answer == "Paper":
+        print ("You win")
 
-else: 
-    print("You loose")
+    # Loose conditions 
+
+    else: 
+        print("You loose")
+
+    # Temporary variable to end the game 
+
+    play_again = input("Would you like to play again> (Y/N)").lower()
+    if not play_again == "y":
+        game_running = False
+
+print("Thanks for playing!")
+
+
+
+
+        
+
+ 
+
