c7c04e69ae5b886e62a5adfa8a85f9930c6921d9
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-03 22:18:29 +0100

    feat: New parameter to display numbers

--- Functions.py
+++ Functions.py
@@ -1,6 +1,12 @@
 # leanring functions 
 
-def happy_birthday():
-    print("Learning functions")
+def happy_birthday(name, age):     #Here "Name acts as temporary place holder"
+    print(f"Learning functions with {name} and he is {age} old")
 
-happy_birthday()    
+  
+# Sending data using the parentheses when calling a function is called arguments 
+happy_birthday("Kev", 20)
+happy_birthday("Rod", 20)
+happy_birthday("Gut", 20)
+
+
