444e19f41dd9631244f10bc760738cc1e44c32dc
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-04 11:29:47 +0100

    feat: New parameters in order to check how the function will handle this paramenters

--- Functions.py
+++ Functions.py
@@ -1,4 +1,5 @@
 # leanring functions 
+# Function are jsut block of code that can be used for one of more cases, re-usable code
 
 def happy_birthday(name, age):     #Here "Name acts as temporary place holder"
     print(f"Learning functions with {name} and he is {age} old")
@@ -8,5 +9,9 @@ def happy_birthday(name, age):     #Here "Name acts as temporary place holder"
 happy_birthday("Kev", 20)
 happy_birthday("Rod", 20)
 happy_birthday("Gut", 20)
+happy_birthday("And", 45)
+
+# In this case the function is giving 4 different outputs depending on the paramentes given at the very begining 
+
 
 
