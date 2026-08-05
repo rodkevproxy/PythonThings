635d943321d9791eea7cc2a0c02262ba0ac89f9b
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-05 13:29:25 +0100

    feat: Examples of how a normal function works with positonal parameters

--- Default_Arguments.py
+++ Default_Arguments.py
@@ -1,1 +1,18 @@
-#Starting the day with default arguments im python
+# default arguments: A defaukt value for certain paramenters
+#                    default i used whem that argument is omitted 
+#                    make your functions more flexible and reduce the number of arguments 
+#                     1. positoinal, 2. Default 3. Keyword 4. Arbutrary
+# Starting the day with default arguments im python
+
+# Here is an example of a function using porsitonal parameters 
+# This is how a normal function works, by giving it the parameters 
+
+def net_price (list_price, discount, tax):
+    return list_price * (1 - discount) * (1 + tax)
+
+print(net_price(200, 0.5, 0.05))
+
+
+
+
+
