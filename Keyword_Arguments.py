7c5410a40751bfbcd9eba7d6c3c0834c1b18a7c8
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-05 23:24:47 +0100

    feat: Examples and differences between keyword arguments and conventional ones

--- Keyword_Arguments.py
+++ Keyword_Arguments.py
@@ -1,2 +1,24 @@
 # Now we are into the keyword arguments 
 
+# Keyword Arguments: An argument preceded by an identifier 
+#                    Helps with readability 
+#                    Order of the arguments does not matter in this case  
+#                     1. positoinal, 2. Default 3. Keyword 4. Arbutrary
+
+def hello(greeting, title, first, last):
+    print(f"{greeting}{title}{first}{last}")
+
+hello("hello", "mr", "kevin", "rodas") #This is a conventional way where if you change the position of the arguments, the position will also change in the output 
+
+
+def hello_keyword(greeting, title, first, last):
+    print(f"{greeting}{title}{first}{last}")
+
+hello_keyword(tittle="mr", name="kevin", last="rodas",greeting="hello ")#Here the output will remain the same, even when the order of the argumnets is different from the order of the function paramenters
+
+
+              
+
+
+
+
