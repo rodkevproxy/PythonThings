7f89f99a233fadb28a8c4b981520603dd5835cc7
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-05 23:38:25 +0100

    feat: NEw function example

--- Keyword_Arguments.py
+++ Keyword_Arguments.py
@@ -8,17 +8,22 @@
 def hello(greeting, title, first, last):
     print(f"{greeting}{title}{first}{last}")
 
-hello("hello", "mr", "kevin", "rodas") #This is a conventional way where if you change the position of the arguments, the position will also change in the output 
+hello("hello ", "mr ", "kevin ", "rodas ") #This is a conventional way where if you change the position of the arguments, the position will also change in the output 
 
 
 def hello_keyword(greeting, title, first, last):
     print(f"{greeting}{title}{first}{last}")
 
-hello_keyword(title="mr", first="kevin", last="rodas", greeting="hello") #Here the output will remain the same, even when the order of the argumnets is different from the order of the function paramenters
+hello_keyword(title="mr ", first="kevin ", last="rodas ", greeting="hello ") #Here the output will remain the same, even when the order of the argumnets is different from the order of the function paramenters
 
+#Function to get a phone number 
 
-              
+def get_phone(countrycode, areacode, first, last):
+    return(f"{countrycode}-{areacode}-{first}-{last}") # An fstring can also be returned without having to put a print function 
 
+phone_number = get_phone(countrycode=44, areacode=0, first=75761, last=91101)
+
+print(phone_number)
 
 
 
