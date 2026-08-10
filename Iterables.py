1863b7b90db907961b0a81f1dfcc7adbdc038395
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-10 15:59:24 +0100

    feat: Update print statements to format output with separators

--- Iterables.py
+++ Iterables.py
@@ -5,5 +5,37 @@
 numbers = [1, 2, 3, 4, 5]
 
 for num in reversed(numbers): 
-    print(num)
+    print(num, end="-")
+
+print()
+
+fruits_tupple = ("Mango", "Fresa", "Banana", "Cocos", "Guanabana")
+
+for fruit in fruits_tupple:
+    print(fruit, end=" ")
+
+print()
+
+fruits = "Mango"
+
+for letter in fruits:
+    print(letter, end=" ")
+
+print()
+
+My_Dictionary = { "A" : 1, "B" : 2, "C" : 3, "D" : 4}
+
+
+for key, value in My_Dictionary.items():
+    print(f"{key} : {value}")
+
+
+
+
+
+
+
+
+
+
 
