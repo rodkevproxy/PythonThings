b42e3d83716db2c889e70dda63f53453e18f16b9
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-13 23:37:53 +0100

    feat: Add example of list comprehension for converting strings to uppercase

--- List_coprehension.py
+++ List_coprehension.py
@@ -16,3 +16,11 @@ doubles_2 = [x *2 for x in range(1, 11)] #This will return the same
 
 print(doubles_2)
 
+#Now working this but with strings 
+
+fruits = ["apple", "banana", "coconut"]
+#Now we have to make all this uppercase
+
+fruits = [fruit.upper() for fruit in fruits]
+
+print (fruits)
