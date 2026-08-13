f79bae778b422cb4b778b30e9797197743d0016c
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-13 23:02:21 +0100

    feat: Add example of list comprehension for generating doubles

--- List_coprehension.py
+++ List_coprehension.py
@@ -10,3 +10,9 @@ for x in range(1, 11):
     doubles.append(x * 2)
 print(doubles)
 
+#Now using [Expresion for value in iterable if condition] (checking a condition is optional)
+
+doubles_2 = [x *2 for x in range(1, 11)] #This will return the same 
+
+print(doubles_2)
+
