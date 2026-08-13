efc40e04ff8c2f7f60a196a4d6129d4a7beaea8a
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-13 23:41:34 +0100

    feat: Add example of list comprehension for extracting first letters from fruits

--- List_coprehension.py
+++ List_coprehension.py
@@ -21,6 +21,15 @@ print(doubles_2)
 fruits = ["apple", "banana", "coconut"]
 #Now we have to make all this uppercase
 
-fruits = [fruit.upper() for fruit in fruits]
-
+fruits = [fruit.upper() for fruit in fruits] #This makes all the words upper case 
 print (fruits)
+
+
+#This one will put the first letter and put it on a new list 
+
+fruits_2 = ["apple", "banana", "coconut"]
+fruit_2 =[fruit[0] for fruit in fruits_2]
+print (fruit_2)
+
+
+
