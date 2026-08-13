a3c7d4166407e61b66653c9c161862a33a281441
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-13 23:00:08 +0100

    feat: Add list comprehension example for generating doubles

---
+++ List_coprehension.py
@@ -0,0 +1,12 @@
+# List coprehension = Is a precise way to create lists in Python
+#                       Compact and easier to read than traditional loops 
+#                       [Expresion for value in iterable if condition]
+
+
+#Traditional way 
+doubles = []
+
+for x in range(1, 11):
+    doubles.append(x * 2)
+print(doubles)
+
