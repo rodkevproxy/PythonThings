0c239c3e8f3dfd72ffa25292e15876d1c641ef75
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-14 23:20:57 +0100

    feat: Add initial module documentation for code organization and usage

---
+++ Module.py
@@ -0,0 +1,4 @@
+# Moddule = a file containing code you want to include in your program 
+#           use "import" to include a module (built in or my own one)
+#           useful to break up a large program reusable separate files 
+
