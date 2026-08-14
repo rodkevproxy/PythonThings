9b36143006c3464691eb9e032977ce4c4d32e8d2
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-14 23:59:34 +0100

    feat: Enhance module documentation with additional comments and examples

--- Module.py
+++ Module.py
@@ -2,3 +2,24 @@
 #           use "import" to include a module (built in or my own one)
 #           useful to break up a large program reusable separate files 
 
+
+# We can give this modules names 
+
+import math as m # This way we can change the name of the module 
+
+print(m.pi)
+
+#To import specific things, we can use the following lines 
+
+from math import pi #This line will import only the pi function 
+
+print(pi) #This way we can use it with any added code 
+
+
+
+
+
+
+
+
+
