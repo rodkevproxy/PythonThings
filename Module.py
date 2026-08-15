90327adbb81741cb1bfed88f7b61afbffdd4f6d6
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-15 20:39:13 +0100

    refactor: Clean up comments and improve code readability in Module.py

--- Module.py
+++ Module.py
@@ -5,15 +5,31 @@
 
 # We can give this modules names 
 
-import math as m # This way we can change the name of the module 
+#import math as m # This way we can change the name of the module 
 
-print(m.pi)
+#print(m.pi)
 
 #To import specific things, we can use the following lines 
 
-from math import pi #This line will import only the pi function 
+from math import pi #This line will import only the pi function (using this can cause name conflicts)
 
 print(pi) #This way we can use it with any added code 
+
+#How to do it properly 
+
+import math # e is an exponensial constant 
+
+a, b, c, d = 1, 2, 3, 4
+
+print(math.e ** a)
+
+
+
+
+
+
+
+
 
 
 
