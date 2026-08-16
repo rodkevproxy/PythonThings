fe91b480b2a31dcc99a45b45157c8650839ada87
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-16 21:25:39 +0100

    feat: Add local variable example in variable_scope_and_scope_resolution.py

--- variable_scope_and_scope_resolution.py
+++ variable_scope_and_scope_resolution.py
@@ -1,3 +1,38 @@
 # Viable scope = where a variable is visible and accesible 
+
 #Scope resolution = (LEGB) Local -> Enclosed -> Global -> Built in
+
+
+
+
+def func1(): 
+    print(x)
+
+def func2():
+    print(x)
+
+x = 10
+
+#Example of a global function 
+
+
+
+
+
+def func1(): 
+    a = 1 # This varible is local to function 1 
+    print(a)
+
+    def func2():
+    b = 2 
+    print(b)
+
+    func2()
+    #Example of a enclosed fuction 
+
+
+#Built in example 
+
+
+
 
