8603309b812002f43793665f12267e8fd2ac4952
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-16 22:41:25 +0100

    feat: Remove unused if_name_==_main_.py and add script2.py

--- if_name_==_main_.py
+++
@@ -1,6 +0,0 @@
-# if _name_ == _main_: (This script can be imported or rin standalone)
-#                       Funcitons and classes in this module can be reused without the 
-#                       main block of code executing 
-
-
-
