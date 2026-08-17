65d68075fbe2728feb9cb89ed12a9208eab52bac
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-17 15:47:39 +0100

    feat: Update script1.py and script2.py with function examples and debugging configuration

--- script1.py
+++ script1.py
@@ -1,2 +1,24 @@
-print(dir())
+
+# from script2 import * # Here __name__ is equals to string script2 
+
+
+print(__name__) #But here __name__ is equals to __main__, meaning we are running script1 directly 
+
+def main():
+    print("This is script 1")
+
+def fav_food(food):
+    print(f"Your favourite food is {food}")
+    fav_food("Pizza")
+    print("Good bye")
+
+
+
+if __name__ == '__manin__';
+    main()
+
+
+
+
+
 
