1c795031aca997d55435b34fe77a9cff72bbfa0a
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-17 15:50:54 +0100

    refactor: Move fav_food function definition above main in script1.py

--- script1.py
+++ script1.py
@@ -4,17 +4,14 @@
 
 print(__name__) #But here __name__ is equals to __main__, meaning we are running script1 directly 
 
+def fav_food(food):
+    print(f"Your favourite food is {food}")
+
 def main():
     print("This is script 1")
     fav_food("Pizza")
     print("Good bye")
     
-
-def fav_food(food):
-    print(f"Your favourite food is {food}")
-    
-
-
 if __name__ == '__main__':
     main()
 
