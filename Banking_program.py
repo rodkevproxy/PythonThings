58ed6518d7024e3833d1e9752fa57ccbab7f103c
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-18 15:04:47 +0100

    feat: Add initial structure for Banking program

---
+++ Banking_program.py
@@ -0,0 +1,15 @@
+def show_balance():
+    pass 
+
+def deposite():
+    pass
+
+def withdraw():
+    pass
+
+balance = 0 
+is_running = True 
+
+while is_running: 
+    print("Banking program")
+    
