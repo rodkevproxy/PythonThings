fcc43ffab4f05b431a083629969ab296d1b07017
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-18 23:04:39 +0100

    feat: Add menu options for banking program interaction

--- Banking_program.py
+++ Banking_program.py
@@ -17,6 +17,14 @@ is_running = True
 
 while is_running: 
     print("Banking program")
+    print("1.Show Balance")
+    print("2.Deposit")
+    print("3.Withdraw")
+    print("4.Exit")
+
+    choice = input("Please select an option (1-4)")
+
+    if option == "1":
 
 
 
