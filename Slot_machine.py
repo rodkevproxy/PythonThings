f64dad21182e200744333f51b84f1f3fe46de281
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-19 23:08:04 +0100

    fix: Correct spelling in the comment header for the slot machine program

--- Slot_machine.py
+++ Slot_machine.py
@@ -1,1 +1,22 @@
-# Begginers guide to create a slot machine 
+# Beginners guide to create a slot machine 
+
+def spin_row(): 
+    pass 
+
+def print_row(): 
+    pass
+
+def pay_out():
+    pass 
+
+def main():
+    balance: 100 
+
+    print("")
+
+if __name__ == '__main__':
+    main()
+
+
+
+
