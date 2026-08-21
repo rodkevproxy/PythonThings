c878586a16f7a591c128b927af33d059264d3e03
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-21 23:58:18 +0100

    fix: Correct comment for character list conversion in encryption program

--- Encryption_program.py
+++ Encryption_program.py
@@ -4,7 +4,7 @@ import string
 chars = " " + string.punctuation + string.digits + string.ascii_letters #Here instead of writing every single character manually, i decided to used specific  sections from the imports 
 #Right now, chars is a long string, now i need to convert every single character into a single string, so i will typecast the entire String in to a list 
 
-chars = list(chars)
+chars = list(chars) #Here all the characters are a single string, insteadd of a single big one
 
 print(chars)
 
