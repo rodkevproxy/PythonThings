c7ace2034e176f1802db9ce2379d1d34d6490269
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-21 22:57:08 +0100

    feat: Add character set for encryption using string module

--- Encryption_program.py
+++ Encryption_program.py
@@ -1,2 +1,6 @@
 import random 
 import string 
+
+chars = string.punctuation + string.digits + string.ascii_letters #Here instead of writing every single character manually, i decided to used specific  sections from the imports 
+print(chars)
+
