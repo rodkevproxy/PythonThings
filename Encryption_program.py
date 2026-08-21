f593c0a9ee1be3320c9156404315316b2e0a5d2c
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-21 23:45:20 +0100

    fix: Add space character to the character set for encryption

--- Encryption_program.py
+++ Encryption_program.py
@@ -1,6 +1,11 @@
 import random 
 import string 
 
-chars = string.punctuation + string.digits + string.ascii_letters #Here instead of writing every single character manually, i decided to used specific  sections from the imports 
+chars = " " + string.punctuation + string.digits + string.ascii_letters #Here instead of writing every single character manually, i decided to used specific  sections from the imports 
+#Right now, chars is a long string, now i need to convert every single character into a single string, so i will typecast the entire String in to a list 
+
+chars = list(chars)
+
 print(chars)
+
 
