0be3f5faee1df05775ca23a4f43ab5272f7e72c6
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-24 22:45:37 +0100

    refactor: Improve formatting and spacing in hangman_art dictionary

--- Hangman_gmae.py
+++ Hangman_gmae.py
@@ -7,29 +7,54 @@ words = ("Apple", "Banana", "Coconut", "Kiwi", "Mango", "Strawberry", "Cherry")
 #Once we reac 6 incorrected guesses we loose the game 
 #Dicionary of key():
 #ASCII art
-hangman_art = {0:("     "
-                  "     "
+hangman_art = {0:("     ",
+                  "     ",
                   "     "), 
-               1:("  o  "
-                  "     "
+               1:("  o  ",
+                  "     ",
                   "     "), 
-               2:("  o  "
-                  "  |  "
+               2:("  o  ",
+                  "  |  ",
                   "     "), 
-               3:("  o  "
-                  "  |\\ "  #The back slash is a escape sequence on a string, se i use two of them to display one
+               3:("  o  ",
+                  "  |\\",  #The back slash is a escape sequence on a string, se i use two of them to display one
                   "     "), 
-               4:("  o  "
-                  " /|\\ "
+               4:("  o  ",
+                  " /|\\",
                   "     "), 
-               5:("  o  "
-                  " /|\\ "
+               5:("  o  ",
+                  " /|\\",
                   " /   "), 
-               6:("  o  "
-                  " /|\\ "
-                  " / \\ ")}
+               6:("  o  ",
+                  " /|\\",
+                  " / \\")}
 
 
+def display_man (wrong_guesses): 
+    pass 
+
+def display_hint (hint):
+    pass
+
+def display_answer(answer): 
+    pass
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
+#Frame x race is using fedex 
+
+
+
+
+
+
+
+    
 
 
 
