1373951860476005395687d0c23a2e70fe8ee450
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-24 21:44:19 +0100

    refactor: Clean up formatting and add spacing in Hangman game file

--- Hangman_gmae.py
+++ Hangman_gmae.py
@@ -1,6 +1,13 @@
+import random
+
 words = ("Apple", "Banana", "Coconut", "Kiwi", "Mango", "Strawberry", "Cherry")
 
 #Now the functions and extra things that we will need for this game
+
+#Once we reac 6 incorrected guesses we loose the game 
+#Dicionary of key():
+#ASCII art
+hangman_art = {0:(), 1:(), 2:(), 3:(), 4:(), 5:(), }
 
 
 
