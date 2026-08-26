ca30264435e11006d9031385259d0eb384f45168
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-26 22:57:44 +0100

    refactor: Clean up Object.py by removing unnecessary comments and print statements

--- Python Object Oriented Programming/Object.py
+++ Python Object Oriented Programming/Object.py
@@ -4,22 +4,16 @@
 
 # Class = (blueprint) used to desing the structure and layout of an object 
 
-from carClass import Car 
-
-
-car1 = Car("Mustang", 2024, "Red", False)
-
-
+#Clases can be re used, here is a quick example by creating a secong car with different specs 
 #print(car1) If we atempt this, what wi will end up getting is the memory address location of where the object is
 
-print(car1.model, car1.year) # This dot is known as the attribute access operator 
 
-#Clases can be re used, here is a quick example by creating a secong car with different specs 
+from carClass import Car 
 
+car1 = Car("Mustang", 2024, "Red", False)
 car2 = Car("GT3", 2027, "Black", True)
 
-print(car2.model, car1.model)
-print(car1.for_sale)
+
 
 car1.drive()
 car2.stop()
