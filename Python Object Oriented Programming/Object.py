18f2bd9a4239fe4720e5689ebc65ee44d152b4be
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-26 21:44:03 +0100

    feat: Update Car class constructor to accept parameters for model, year, colour, and for_sale

--- Python Object Oriented Programming/Object.py
+++ Python Object Oriented Programming/Object.py
@@ -5,5 +5,18 @@
 # Class = (blueprint) used to desing the structure and layout of an object 
 
 class Car: 
-    def __init__(self): #This is the constructor method, this is required in order to construct objects 
-        
+    def __init__(self, model, year, colour, for_sale): #This is the constructor method, this is required in order to construct objects 
+        self.model = model 
+        self.year = year
+        self.colour = colour 
+        self.for_sale = for_sale 
+
+car1 = Car("Mustang", 2024, "Red", False)
+
+
+#print(car1) If we atempt this, what wi will end up getting is the memory address location of where the object is
+
+print(car1.model, car1.year) # This dot is known as the attribute access operator 
+
+
+
