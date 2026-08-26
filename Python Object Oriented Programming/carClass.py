3a7cca0b265d50afa1f01897e67d8bb0105e1dce
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-26 22:51:44 +0100

    feat: Add drive and stop methods to Car class for enhanced functionality

--- Python Object Oriented Programming/carClass.py
+++ Python Object Oriented Programming/carClass.py
@@ -1,8 +1,16 @@
-# Classes can take a lot off space, so for better organization it is convinient to put them on separate files
+# Classes can take a lot off space, so for better organisation it is convinient to put them on separate files
 
 class Car: 
     def __init__(self, model, year, colour, for_sale): #This is the constructor method, this is required in order to construct objects 
         self.model = model 
         self.year = year
         self.colour = colour 
-        self.for_sale = for_sale 
+        self.for_sale = for_sale
+
+    #Methods are actions that our class can perform
+    def drive(self):
+        print("You drive the car ")
+
+    def stop(self):
+        print("You can stop the car")
+
