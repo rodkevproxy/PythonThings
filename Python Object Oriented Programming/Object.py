c5908ad98134353c3b4fb1efaad7f121775858e9
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-26 16:39:55 +0100

    feat: Add initial Car class and related comments in Object.py

---
+++ Python Object Oriented Programming/Object.py
@@ -0,0 +1,9 @@
+# object = A "bundle" of related attributes (Variables) and methods (functions) 
+# Ex. phone, cup, book
+# You need a class to create many objects 
+
+# Class = (blueprint) used to desing the structure and layout of an object 
+
+class Car: 
+    def __init__(self): #This is the constructor method, this is required in order to construct objects 
+        
