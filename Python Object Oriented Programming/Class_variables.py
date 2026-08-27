85ece31b9bfd57002be46fc016ef7bcb98865af8
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-27 14:16:28 +0100

    feat: Add Student class with constructor to initialize name and age attributes

--- Python Object Oriented Programming/Class_variables.py
+++ Python Object Oriented Programming/Class_variables.py
@@ -2,3 +2,8 @@
 #                   Defined outside the constructor 
 #                   Allow you to share data among all objetcs created from that class 
 
+class Student: 
+    def __init__(self, name, age): 
+        self.name = name 
+        self.age = age 
+        
