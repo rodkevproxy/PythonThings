36aefbad0119bfd70c71238712602ace8079625d
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-27 14:21:06 +0100

    refactor: Clean up Class_variables.py by adding spacing and comments for clarity

--- Python Object Oriented Programming/Class_variables.py
+++ Python Object Oriented Programming/Class_variables.py
@@ -3,7 +3,19 @@
 #                   Allow you to share data among all objetcs created from that class 
 
 class Student: 
+
+    #Class variable. These are difined outside the constructor
+
+
+
     def __init__(self, name, age): 
-        self.name = name 
+        self.name = name #These are instance variables
         self.age = age 
-        
+
+student1 = Student("Spongebob", 30)
+student2 = Student("Bob", 19)
+print(student1.name)
+print(student2.age)
+
+
+
