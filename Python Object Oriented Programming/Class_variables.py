b44803ca83710017d7cdee432dd91a5bb9cbe84f
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-27 14:02:08 +0100

    feat: Add Class_variables.py to define and demonstrate class variables

---
+++ Python Object Oriented Programming/Class_variables.py
@@ -0,0 +1,4 @@
+# Class variables = Shared among all instances of a class 
+#                   Defined outside the constructor 
+#                   Allow you to share data among all objetcs created from that class 
+
