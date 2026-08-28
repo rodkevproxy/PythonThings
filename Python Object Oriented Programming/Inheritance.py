dc0be696d00c18d62708e3068724a3dea02fd71b
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-28 14:51:59 +0100

    feat: Refactor Animal class and add methods for eating and sleeping

--- Python Object Oriented Programming/Inheritance.py
+++ Python Object Oriented Programming/Inheritance.py
@@ -1,4 +1,31 @@
 # Inheritance = Allows a class to inherit attributes and methods from another class 
 #               Helps with code reusability and extensibility 
 #               Class Child(Parent)
+# This allows you to usew attributes and methods from another class 
+
+class Animal: 
+    def __init__(self, name): #This is how you define the contructor 
+        self.is_alive = True
+        self.name = name 
+
+    def eat(self):
+        print(f"{self.name} is eating")
+
+    def sleep(self):
+        print(f"{self.name} is sleeping")
+
+class Dog(Animal):
+     pass 
+
+class Cat(Animal): 
+    pass 
+
+class Mouse(Animal): 
+    pass 
+
+dog = Dog("Dollar")
+cat = Cat("Pepe")
+mouse =Mouse("Jerry")
+
+
 
