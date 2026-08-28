702d381449460b190dbebd96f4ec47bb4719dc18
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-28 11:05:23 +0100

    feat: Add comments to explain inheritance concept in Inheritance.py

---
+++ Python Object Oriented Programming/Inheritance.py
@@ -0,0 +1,3 @@
+# Inheritance = Allows a class to inherit attributes and methods from another class 
+#               Helps with code reusability and extensibility 
+#               Class Child(Parent)
