785a2493630138cd3477679112ec2241c554195e
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-29 00:08:57 +0100

    feat: Add initial classes for multilevel inheritance example

---
+++ Python Object Oriented Programming/Multiple/Multilevel_Inheritance.py
@@ -0,0 +1,23 @@
+# multiple inheritance = inherit from more than one parent class 
+#                         C(A,B)
+
+
+# multilevel inheritance = inherit from a pareent which inherits from another parent 
+#                           C(B) <- B(A) <- A 
+
+
+class Prey: 
+    pass 
+
+class Predator: 
+    pass 
+
+class Rabbit: 
+    pass 
+
+class Hawk: 
+    pass 
+
+class Fish: 
+    pass 
+
