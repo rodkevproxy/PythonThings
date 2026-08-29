3e79f63c9651b4d457c00c9d72e2c478cee84818
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-29 21:48:18 +0100

    feat: Add Circle, Square, and Triangle classes with constructors for shape properties

--- Python Object Oriented Programming/Super()_class.py
+++ Python Object Oriented Programming/Super()_class.py
@@ -1,4 +1,25 @@
 # super() = Function used in a child class to call methods from a parent class (superclass).
 #           Allows you to extend the functionality of the inherited methods 
+class Circle:
+     def __init__(self, colour, filled, radious):
+          self.colour = colour 
+          self.filled = filled 
+          self.radious = radious 
+          
+
+class Square:
+     def __init__(self, colour, filled, width):
+               self.colour = colour 
+               self.filled = filled 
+               self.width = width 
+
+class Triangle:
+     def __init__(self, colour, filled, width, height):
+               self.colour = colour 
+               self.filled = filled 
+               self.width = width 
+               self.height = height 
+
+
 
 
