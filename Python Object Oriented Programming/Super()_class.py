d5a3542e556239ed6546493b07636082573387f5
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-29 21:49:25 +0100

    fix: Correct parameter name from 'filled' to 'is_filled' in Circle, Square, and Triangle class constructors

--- Python Object Oriented Programming/Super()_class.py
+++ Python Object Oriented Programming/Super()_class.py
@@ -1,22 +1,22 @@
 # super() = Function used in a child class to call methods from a parent class (superclass).
 #           Allows you to extend the functionality of the inherited methods 
 class Circle:
-     def __init__(self, colour, filled, radious):
+     def __init__(self, colour, is_filled, radious):
           self.colour = colour 
-          self.filled = filled 
+          self.is_filled = is_filled 
           self.radious = radious 
           
 
 class Square:
-     def __init__(self, colour, filled, width):
+     def __init__(self, colour, is_filled, width):
                self.colour = colour 
-               self.filled = filled 
+               self.is_filled = filis_filledled 
                self.width = width 
 
 class Triangle:
-     def __init__(self, colour, filled, width, height):
+     def __init__(self, colour, is_filled, width, height):
                self.colour = colour 
-               self.filled = filled 
+               self.is_filled = is_filled 
                self.width = width 
                self.height = height 
 
