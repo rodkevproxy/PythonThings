650f178dccb1b6807b2df2c6476520748fd61c99
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-30 17:33:59 +0100

    feat: Add describe method to Shape class and implement height attribute in tempCodeRunnerFile

--- Python Object Oriented Programming/Super()_class.py
+++ Python Object Oriented Programming/Super()_class.py
@@ -5,6 +5,10 @@ class Shape:  #This is the super class, dont forget to setup this class into the
        def __init__(self, colour, is_filled): 
               self.colour = colour 
               self.is_filled = is_filled 
+
+       def describe(self):
+              print(f"It is {self.colour} and {"filles" if self.filled == True else "Not Filled"}")
+              
               
 
 
@@ -33,11 +37,8 @@ circle = Circle("blue", True, 5) #No keyword arguments
 square = Square(colour="Blue", is_filled=False, width=90) #With keyword argumnets 
 triangle = Triangle(colour="Red", is_filled=True, width=23, height=20) 
 
-print(circle.colour)
-print(circle.is_filled)
-print(triangle.height)
 
-
+circle.describe()
 
 
 
