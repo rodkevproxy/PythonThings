1cb41bfa9b5dda2e2b727aae83d908dfa51b981b
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-31 23:19:49 +0100

    feat: Add initial comments explaining Polymorphism concepts and methods

---
+++ Python Object Oriented Programming/Polymorphism.py
@@ -0,0 +1,8 @@
+#Polymorphism = Greek word that means ti "have any forms or faces"
+#                               poly = Many
+#                               morphe = Form 
+#                           
+#                           TWO WAYS TO ACHIEVE POLYMORPHISM 
+#                          1. Inheritance = An object could be treated the same type as a parent class 
+#                          2. "Duck Typing" = Object must have necessary attribute/methods
+
