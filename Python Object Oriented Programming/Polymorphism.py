bc4353269ab5e03cbecf59e620d83c8d35d3156f
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-31 23:21:51 +0100

    feat: Add Shape, Circle, Square, and Triangle classes for Polymorphism examples

--- Python Object Oriented Programming/Polymorphism.py
+++ Python Object Oriented Programming/Polymorphism.py
@@ -8,3 +8,16 @@
 
 #On this file we will be more focused on the first one 
 
+class Shape():
+    pass 
+
+class Circle(Shape): 
+    pass 
+
+class Square(Shape):
+    pass 
+
+class Triangle(Shape):
+    pass
+
+square = Square()
