2bfcc4a081d09414c1a9f4f4baf99d618288e634
rodkevproxy <kevinrodasuk@gmail.com>
2026-08-31 23:20:11 +0100

    chore: Add clarification comment on focus of Polymorphism implementation

--- Python Object Oriented Programming/Polymorphism.py
+++ Python Object Oriented Programming/Polymorphism.py
@@ -6,3 +6,5 @@
 #                          1. Inheritance = An object could be treated the same type as a parent class 
 #                          2. "Duck Typing" = Object must have necessary attribute/methods
 
+#On this file we will be more focused on the first one 
+
