d46a136c86d0a66078e0c724b440c3de55163c54
rodkevproxy <kevinrodasuk@gmail.com>
2026-09-04 23:46:38 +0100

    chore: Add StaticMethods.py with explanations of static and instance methods

---
+++ Python Object Oriented Programming/StaticMethods.py
@@ -0,0 +1,6 @@
+# Static Methods = A method that belong to a class rather than any object from that class (instance) 
+#                   Usually used for functions 
+
+# Instance Methods = Best for operations on instances of the class (objects)
+# Static Methods = Best for utility functions that do not need access to class data 
+
