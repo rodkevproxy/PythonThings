3c9782ea497ca7fadf1182bba189d23906b2cdf4
rodkevproxy <kevinrodasuk@gmail.com>
2026-09-08 23:21:14 +0100

    chore: Remove unnecessary newline in __str__ method and add equality check in example usage

--- Python Object Oriented Programming/MagicMethods.py
+++ Python Object Oriented Programming/MagicMethods.py
@@ -11,7 +11,6 @@ class Book:
 
     def __str__(self):  #This method will change the message given whne printing an object 
         return f"'{self.title}' by {self.author}"
-
     def __eq__(self, other): #This method will return if 2 "books in this case" are equal
         return self.title == other.title and self.author == other.author
     def __lt__(self, other): #Basically this allow python to compare two objects using the "<"  symbol 
@@ -23,3 +22,4 @@ book2 = Book("The oddysey", "The Guy", 330)
 
 print(book2)
 print(book1 < book2)
+print(book1 == book2)
