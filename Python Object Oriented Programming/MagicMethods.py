07fa5a1d79c747c2e4211cf9e50202e76ec90755
rodkevproxy <kevinrodasuk@gmail.com>
2026-09-08 16:24:54 +0100

    chore: Add initial comments explaining magic methods in MagicMethods.py

---
+++ Python Object Oriented Programming/MagicMethods.py
@@ -0,0 +1,7 @@
+# Magic Methods = Dunder methods (Double undescore) __init__, __str__,__eq__
+#                 They are automatically called by mmany of Python's built in operations
+#                 They allow developers to define or customize the behavior of objects 
+
+
+
+
