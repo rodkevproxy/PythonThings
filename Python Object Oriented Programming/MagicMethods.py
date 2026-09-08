0bb1489a18f65cdd86dc5efd3e1b1004079ccede
rodkevproxy <kevinrodasuk@gmail.com>
2026-09-08 22:42:25 +0100

    chore: Define Book class with __init__ and __str__ methods

--- Python Object Oriented Programming/MagicMethods.py
+++ Python Object Oriented Programming/MagicMethods.py
@@ -3,5 +3,12 @@
 #                 They allow developers to define or customize the behavior of objects 
 
 
+class Book: 
+    def __init__(self, title, author, num_pages):
+        self.title = title 
+        self.author = author 
+        self.num_pages = num_pages
 
+    def __str__(self):
+        return f"'{}' by {}"
 
