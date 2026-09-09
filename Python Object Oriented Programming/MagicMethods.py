9b572e1ed799d4bb095b794021a8d13978a30f07
rodkevproxy <kevinrodasuk@gmail.com>
2026-09-09 21:37:21 +0100

    chore: Implement __getitem__ method in Book class for author retrieval

--- Python Object Oriented Programming/MagicMethods.py
+++ Python Object Oriented Programming/MagicMethods.py
@@ -26,6 +26,12 @@ class Book:
 
     def __contains__(self, keyword):
         return keyword in self.title or self.author 
+    def __getitem__(self, key):
+        if key == "author":
+            return self.author
+        if key 
+        
+
 
 
 
