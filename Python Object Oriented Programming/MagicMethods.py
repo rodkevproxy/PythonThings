b4ea0be7f9620435d88dc3a7935b9d89c8a5b62a
rodkevproxy <kevinrodasuk@gmail.com>
2026-09-09 13:07:52 +0100

    chore: Fix duplicate __lt__ method in Book class to ensure correct comparison behavior

--- Python Object Oriented Programming/MagicMethods.py
+++ Python Object Oriented Programming/MagicMethods.py
@@ -11,10 +11,13 @@ class Book:
 
     def __str__(self):  #This method will change the message given whne printing an object 
         return f"'{self.title}' by {self.author}"
+    
     def __eq__(self, other): #This method will return if 2 "books in this case" are equal
         return self.title == other.title and self.author == other.author
+    
     def __lt__(self, other): #Basically this allow python to compare two objects using the "<"  symbol 
         return self.num_pages < other.num_pages
+    
     def __lt__(self, other): #Basically this allow python to compare two objects using the "<"  symbol 
         return self.num_pages > other.num_pages
 
