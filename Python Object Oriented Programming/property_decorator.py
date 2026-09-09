79a3f44da156dce467914d83ae9a728878a11f55
rodkevproxy <kevinrodasuk@gmail.com>
2026-09-09 22:27:09 +0100

    chore: Add initial documentation for Property decorator in property_decorator.py

---
+++ Python Object Oriented Programming/property_decorator.py
@@ -0,0 +1,4 @@
+# @Property = Decorator to define a method as a property (it can be accessed like an attribute)
+#               Benefit: Add additional logic when read, write, or delete attributes 
+#               Gives you getter, setter and deleter methods 
+
