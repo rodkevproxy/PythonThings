59d592e7f87be98a6bb27b7d1e1f3b273404270c
rodkevproxy <kevinrodasuk@gmail.com>
2026-09-10 21:34:30 +0100

    chore: Add initial comments explaining the purpose of decorators

---
+++ Python Object Oriented Programming/Decorator.py
@@ -0,0 +1,4 @@
+#Decotator = A function that extends the behaviour of another function 
+#           w/o mofifying the base function 
+#           pass the base fucntion as an argument to the decorator 
+
