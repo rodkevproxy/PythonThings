6952c22943a836cc785b1aeb478c28c86f1b00ab
rodkevproxy <kevinrodasuk@gmail.com>
2026-09-10 11:45:23 +0100

    chore: Fix height property implementation and correct setter methods in Rectangle class

--- Python Object Oriented Programming/property_decorator.py
+++ Python Object Oriented Programming/property_decorator.py
@@ -11,12 +11,26 @@ class Rectangle:
         return f"{self._width:.1f}cm"
 
     @property
-    def height(pass): 
-        pass 
+    def height(self): 
+        return f"{self._height:.1f}cm"
+
+    @width.setter 
+    def new_width(self, new_width): 
+        if new_width > 0: 
+            self._width = new_width
+        else: 
+            print("Width must be greater than 0")
+
+    
+    @height.setter 
+    def new_heigh(self, new_height): 
+        if new_height > 0: 
+            self._height = new_height
+        else: 
+            print("Height must be greater than 0")
 
 
-
-    rectangle = Rectangle(3, 4)
+    #rectangle = Rectangle(3, 4)
 
     print(rectangle.width)
     print(rectangle.height)
