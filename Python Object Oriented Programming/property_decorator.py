60bfb8246d5630bfc4a7a4db9d49a526d012168c
rodkevproxy <kevinrodasuk@gmail.com>
2026-09-10 11:35:58 +0100

    chore: Fix initialization of width in Rectangle class and add height property

--- Python Object Oriented Programming/property_decorator.py
+++ Python Object Oriented Programming/property_decorator.py
@@ -4,14 +4,23 @@
 
 class Rectangle:
     def __init__(self, width, height): 
-        self.width = self.width
-        self.height = height
+        self._width = self.width  #To make attributes private or meant to be protected, prefix the attributes with "_"
+        self._height = height
+    @property
+    def width(self): 
+        return f"{self._width:.1f}cm"
+
+    @property
+    def height(pass): 
+        pass 
+
+
 
     rectangle = Rectangle(3, 4)
 
     print(rectangle.width)
     print(rectangle.height)
 
-    #
     
+
  
