343dec466823c5939cb21dc9b0aa17ddfec6cbd3
rodkevproxy <kevinrodasuk@gmail.com>
2026-09-16 11:31:28 +0100

    chore: Add decorator example with add_topping function to enhance ice cream output

--- Python Object Oriented Programming/Decorator.py
+++ Python Object Oriented Programming/Decorator.py
@@ -2,6 +2,20 @@
 #           w/o mofifying the base function 
 #           pass the base fucntion as an argument to the decorator 
 
+#Here is how to create a decorator 
+def add_topping(func): 
+    def wrapper():
+        print("You added sprinkles")
+        func()
+    return wrapper 
+
+
+@add_topping
 def get_ice_cream(): 
     print("Here is your ice cream")
+
+get_ice_cream()
+
+
+
 
